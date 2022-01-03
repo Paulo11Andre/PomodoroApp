985f872027af6e148e69f81c1b80a63e7dc2da6a
Paulo Valarezo <paulinho.ec.1@gmail.com>
2022-01-03 00:22:55 -0500

    Versión estable de la app. Todas las pantallas transicionan correctamente. Sin login real ni creación de usuario real

--- app/src/main/java/com/example/pomodoro/CrearCuentaActivity.java
+++ app/src/main/java/com/example/pomodoro/CrearCuentaActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Objects;
+
 public class CrearCuentaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 
     public void crearCuenta(View view){
